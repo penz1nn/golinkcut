3c69956676515631723dce953eed19bc1fbd4a92
Никита Пензин <penz1n.n@ya.ru>
2023-12-22 13:35:06 +0300

    Add comments for package entity

--- internal/entity/link.go
+++ internal/entity/link.go
@@ -1,6 +1,14 @@
+// Package entity contains the main domain entities (data models) of the
+// core business logic
 package entity
 
+// Link is the main entity which represents a shortened link
 type Link struct {
-	Alias    string `json:"alias"`
+	// Alias is by specification a 10-character string which is used in the
+	// short versin of the link
+	Alias string `json:"alias"`
+
+	// Original represents the original URL which has a corresponding short
+	// version
 	Original string `json:"original"`
 }
